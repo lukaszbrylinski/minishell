9c53beba5d312a4fd2d1d73e31bda1c37711d538
dszafran <dszafran@st.swps.edu.pl>
2025-06-28 17:57:17 +0200

    Modified create_token, and token adding functions. Added list_init, and functions to free and delete node and list

--- include/minishell.h
+++ include/minishell.h
@@ -6,7 +6,7 @@
 /*   By: mika <mika@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/26 15:43:37 by dszafran          #+#    #+#             */
-/*   Updated: 2025/06/27 15:30:48 by mika             ###   ########.fr       */
+/*   Updated: 2025/06/28 17:23:43 by mika             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,12 @@
 # include <readline/readline.h>
 # include <readline/history.h>
 # include "libft.h"
+
+// #define List_count(A) ((A)->count)
+// #define List_first(A) ((A)->first != NULL ? (A)->first->value : NULL)
+// #define List_last(A) ((A)->last != NULL ? (A)->last->value : NULL)
+// #define LIST_FOREACH(L, S, M, V) ListNode *_node = NULL; \
+// for(V = _node = L->S; _node != NULL; V= _node = _node->M)
 
 typedef enum {CMND, WORD, RDIR, PIPE} t_type; // change types? (add file?)
 
--- src/tokenizer.c
+++ src/tokenizer.c
@@ -1,60 +1,117 @@
+// add header
+
 #include "minishell.h"
 
-t_token *create_token(char *str, t_token *previous, t_type type) // pss next also?
+t_token *create_token(char *str, t_token *previous, t_type type) // do I need a function to create tokens with set next?
 {
-	t_token token;
+	t_token *token;
 
-	token.token = str;
-	token.type = type;
-    token.previous = previous;
-	token.next = NULL;
-	return (&token);
+    token = malloc(sizeof(t_token));
+    if (!token)
+        return (NULL);
+	token->token = ft_strdup(str);
+	token->type = type;
+    token->previous = previous;
+	token->next = NULL;
+	return (token);
+}
+
+t_list *list_init()
+{
+    t_token_list *list;
+
+    list = malloc(sizeof(list));
+    list->first = NULL;
+    list->last = NULL;
+    list->size = 0;
+    return (list);
 }
 
 size_t add_back(t_token_list *list, t_token *token) //or keep it void?
 {
     if (!token)
         return ;
-    if (!list)
+    if (list->size == 0)
     {
-        list->size = 1;
         list->first = token;
         list->last = token; //should it be NULL?
     }
     else
     {
-        list->size++;
         token->previous = list->last;
         token->next = NULL; //should it be start?
         list->last = token;
     }
-    return(list->size)
+    list->size++;
+    return(list->size);
 }
 
 size_t add_front(t_token_list *list, t_token *token)
 {
     if (!token)
         return ;
-    if (!list)
+    if (list->size == 0)
     {
-        list->size = 1;
         list->first = token;
         list->last = token; //should it be NULL?
     }
     else
     {
-        list->size++;
         token->next = list->first;
         token->previous = NULL; //should it be last?
         list->first = token;
     }
-    return(list->size)
+    list->size++;
+    return(list->size);
 }
 
-size_t delete_token(t_token_list *list, t_token *token)
+void free_delete_first(t_token_list *list)
+{
+    t_token *temp;
+
+    if (!list || !list->first)
+        return ;
+    temp = list->first;
+    list->size--;
+    list->first = list->first->next;
+    if (list->first != NULL)
+        list->first->previous = NULL;
+    free(temp->token);
+    free(temp);
+}
+
+void list_del_free(t_token_list *list)
+{
+    t_token *current;
+    if (!list)
+        return ;
+    if (!list->first)
+        return (free(list));
+    current = list->first;
+    while (current != NULL)
+    {
+        current = current->next;
+        free_delete_first(list);
+    }
+    free(list);
+}
+
+void print_node(t_token *token)
+{
+
+}
+
+// do I need a function to remove specific token? maybe...
+
+size_t remove_front(t_token_list *list, t_token *token) // do I need a function to delete single token?
 {
     // should I match token based on address? there can be multiple wih the same containts
     
+}
+
+size_t remove_back(t_token_list *list, t_token *token)
+{
+
 }
 
 // no need - I keep size in structure and update inside functions

a22e0c8dfdcb0d62ab3fc06b4eff3ccf8379b311
dszafran <dszafran@st.swps.edu.pl>
2025-07-04 16:47:18 +0200

    cleaned working initial version of tokenizer

--- src/tokenizer.c
+++ src/tokenizer.c
@@ -1,7 +1,5 @@
 // add header
 
-// enum for state?
-
 #include "minishell.h"
 
 t_type detect_type(char c)
@@ -75,73 +73,4 @@ void tokenizer(char *cl_input, t_token_list *token_list)
 		}
 		i += len - 1;
 	}
-	//why it doesn't work correctly with check for SEP?
 }
-
-//while (type == detect_type)
-// i++;
-
-void get_tokens(char *cl_input, t_token_list *token_list) //make separate functions for token types
-{
-	int i;
-	int j;
-	t_type type;
-	t_token *token;
-
-	i = -1;
-	while (cl_input[++i])
-	{
-		j = i;
-		if (ft_strchr(" \t\n\v\r\f", cl_input[i]))
-			continue ;
-		else if (ft_strchr("<>", cl_input[i]))
-		{	
-			type = RDIR;
-			if (ft_strchr("<>", cl_input[i + 1]))
-				j++;
-		}
-		else if (cl_input[i] == '|')
-			type = PIPE;
-		else
-		{	
-			while(!ft_strchr(" \t\n\v\r\f<>|", cl_input[j + 1]))
-				j++;
-			type = CMND;
-		}
-		// but I need to also check for WORD - more tricky due to missing " / '
-		// for sure I need separate functions for types, maybe I should use enum
-		token = create_token(ft_strndup(&cl_input[i], j-i + 1), type);
-		add_back(token_list, token);
-		i = j;
-	}
-}
-
-// t_token *tokenizer(char *cl_input) //test it!
-// {
-// 	int	i;
-// 	int j;
-// 	t_type type;
-// 	t_token *token = NULL;
-
-// 	i = -1;
-// 	while (cl_input[++i])
-// 	{
-// 		j = i;
-// 		if (ft_strchr(" \t\n\v\r\f", cl_input[i]))
-// 			continue ;
-// 		else if (ft_strchr("<>", cl_input[i]))
-// 			type = RDIR;
-// 			if (ft_strchr("<>", cl_input[i + 1]))
-// 				j++;
-// 		else if (cl_input[i] == '|')
-// 			type = PIPE;
-// 		else
-// 		{	
-// 			while(!ft_strchr(" \t\n\v\r\f<>|", cl_input[j + 1]))
-// 				j++;
-// 			type = WORD;
-// 		}
-// 		token = create_token(ft_strndup(cl_input[i], j), type);
-// 	}
-// 	return (token);
-// } // create separate functions for iterating through specific token type and incrementing *j
